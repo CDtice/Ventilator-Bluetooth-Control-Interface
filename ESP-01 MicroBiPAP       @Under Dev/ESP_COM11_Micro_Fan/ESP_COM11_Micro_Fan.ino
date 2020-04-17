c0077f8141e7cf935a27fcaca9719780fb68dcce
CDtice <63695731+CDtice@users.noreply.github.com>
2020-04-17 18:20:19 -0400

    Update ESP_COM11_Micro_Fan.ino

--- ESP-01 MicroBiPAP       @Under Dev/ESP_COM11_Micro_Fan/ESP_COM11_Micro_Fan.ino
+++ ESP-01 MicroBiPAP       @Under Dev/ESP_COM11_Micro_Fan/ESP_COM11_Micro_Fan.ino
@@ -3,7 +3,7 @@
 /* ESP-01 with Micro Fan Simple Timed Breath Cycle
   
 
-// the setup function runs once when you press reset or power the board
+// Fixed Breath Cycle
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);  // Hooked to micro fan
@@ -11,8 +11,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the Fan on (HIGH is the voltage level)
+  delay(2000);                       // wait for 2 second inhale
+  digitalWrite(LED_BUILTIN, LOW);    // turn the Fan off by making the voltage LOW
+  delay(2000);                       // wait for 2 second exhale
 }
